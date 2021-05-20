b13a1a961dc6cb0782a981ea15fc4db18148d9b7
vitor paulo <virtorpaulo333019@gmail.com>
2021-05-20 17:46:50 -0300

    create entities

---
+++ Provider.Domain/Entities/Company.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Provider.Domain.Entities
+{
+    public class Company
+    {
+        public Guid Id { get; protected set; }
+        public bool Active { get; protected set; }
+    }
+}
---
+++ Provider.Domain/Enums/UF.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Provider.Domain.Enums
+{
+    class UF
+    {
+    }
+}
